2c059b6c65ea3ae9803c788a0483b6a9385a481d
Michal Nawrot <m.nawrot@desiter.pl>
2019-04-16 13:09:01 +0200

    improved test legibility

--- src/consumer.ts
+++ src/consumer.ts
@@ -2,11 +2,14 @@ import { Consumer } from "sqs-consumer";
 import config from "./config";
 import logger from "./logger";
 
+const DEFAULT_SQS_BATCH_SIZE = 1;
+
 const consumer = Consumer.create({
+  batchSize: parseInt(config.AWS_SQS_CONSUMER_BATCH_SIZE, 10) || DEFAULT_SQS_BATCH_SIZE,
   queueUrl: config.SQS_QUEUE_URL,
 
   handleMessage: async (message) => {
-    logger.info("receiver message", message);
+    logger.info(`receiver message: ${message.Body}`);
   },
 });
 
--- src/producer.ts
+++ src/producer.ts
@@ -1,22 +1,46 @@
+import crypto from "crypto";
 import { create } from "sqs-producer";
 import { v4 as uuid } from "uuid";
 import config from "./config";
 import logger from "./logger";
 
+const DEAFAULT_NUMBER_OF_GROUPS = 3;
+const DEAFAULT_NUMBER_OF_MESSAGES = 10;
+
 const producer = create({
   queueUrl: config.SQS_QUEUE_URL,
 });
 
-producer.send([
-  {
-    id: uuid(),
+async function sendMessage(messageNo: number, groupNo: number) {
+  return new Promise((resolve, reject) => {
+    const id = uuid();
+    const body = `Group #${groupNo} Message #${messageNo}`;
+    const groupId = `test-group-${groupNo}`;
+    const deduplicationId = crypto.createHash("md5")
+      .update(`${config.TEST_DEDUPLICATION_ID_PREFIX} - ${body}`, "utf8").digest("hex");
 
-    body: `Hello world! Now is ${Date.now()}`,
-    groupId: "test"
-  }], (error: any) => {
-  if (error) {
-    logger.error("Failed to send message: ", error);
-    return;
-  }
-  logger.info("Message sent.");
+    producer.send([{ id, body, groupId, deduplicationId }], (error: any) => {
+    if (error) {
+      logger.error("Failed to send a message: ", error);
+      reject();
+      return;
+    }
+
+    logger.info(`Group #${groupNo} Message #${messageNo} sent.`);
+    resolve();
+  });
 });
+}
+
+const numberOfGroups = parseInt(config.TEST_NUMBER_OF_GROUPS, 10) || DEAFAULT_NUMBER_OF_GROUPS;
+const numberOfMessages = parseInt(config.TEST_NUMBER_OF_MESSAGES, 10) || DEAFAULT_NUMBER_OF_MESSAGES;
+
+async function produce() {
+  for (let groupNo = 1; groupNo <= numberOfGroups; groupNo++) {
+    for (let messageNo = 1; messageNo <= numberOfMessages; messageNo++) {
+      await sendMessage(messageNo, groupNo);
+    }
+  }
+}
+
+produce();
